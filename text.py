0758d9b95e1066fd301232b3b41ea6865d3407c1
WeisongQi <qiweisong@outlook.com>
2025-01-02 14:49:20 +0100

    Add files via upload

---
+++ text.py
@@ -0,0 +1,47 @@
+# user1 = {"password": "1234", "email": "1234@qq.com"}
+# user2 = {"password": "3456", "email": "234@qq.com"}
+
+# user_list = [user1, user2]
+
+# print(user_list[0]["password"])
+# print(user_list[1]["email"])
+# print(user_list[0].keys())
+# print(user_list[0].values())
+# print(user_list[0].items())
+
+# for i in range(5):
+#    print(i)
+
+# list1 = [1, 2, 3, 4, 5]
+
+# # sum = list1[0] + list1[1] + list1[2] + list1[3] + list1[4]
+
+# print(sum(list1))
+
+# garad_list1 = []
+# ungerade_list1 = []
+
+# for i in range(len(list1)):
+#     if list1[i] % 2 == 0:
+#         garad_list1.append(list1[i])
+#     else:
+#         ungerade_list1.append(list1[i])
+#         print(f"{list1[i]} ist ungerade")
+# print(garad_list1)
+# print(ungerade_list1)
+
+
+# max_value = max(list1)
+# print(f"Der größte Wert in list1 ist: {max_value}")
+
+
+# def ungerade(list1):
+#     return [x for x in list1 if x % 2 != 0]
+
+
+# print(ungerade(list1))
+
+thislist = [100, 50, 65, 82, 23]
+print(thislist)
+thislist.sort(reverse=False)
+print(thislist)
